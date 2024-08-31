44d212d79506aa06809fee6347de7b4da8a4a2d1
Muhammad Umer <rawalaumer@gmail.com>
2024-09-01 00:30:40 +0500

    first

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Canvas } from "@react-three/fiber"
+import "./style.css";
+import { Environment, OrbitControls, ScrollControls } from "@react-three/drei";
+import MacContainer from "./MacContainer.js";
+
+const App = () => {
+  return (
+    <Canvas camera={{ fov: 10, position: [0, -10, 220] }}>
+      <OrbitControls />
+      <Environment files={["./studio_small_09_2k.hdr"]} />
+      <ScrollControls pages={3}>
+        <MacContainer />
+      </ScrollControls> 
+    </Canvas>
+  );
+}
+
+export default App
